f57d5a476428d5a74d15b41ab83df8ec4b622eb8
supuniuthpala <48378642+supuniuthpala@users.noreply.github.com>
2020-09-15 23:30:32 +0530

    Commit

--- LogAnalyzerApp/src/com/log_Analyzer/PathObtainer.java
+++ LogAnalyzerApp/src/com/log_Analyzer/PathObtainer.java
@@ -3,12 +3,19 @@ package com.log_Analyzer;
 import java.util.Scanner;
 
 public class PathObtainer {
-    public getPath(String filePath) {
-        Scanner scan = new Scanner(System.in);
 
+    public getPath(String filePath) {
+
+        Scanner scan = new Scanner(filePath);
         System.out.println("Enter the file path:");
         String filePath = scan.nextLine();
 
-        System.out.println(filePath);
+        if(!(filePath.length()==0)){
+            return scan.next();
+        }
+        else{
+            return "";
+        }
+
     }
 }
