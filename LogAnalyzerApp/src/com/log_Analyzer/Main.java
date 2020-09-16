7278312a9867e988a21605223d1c91f3ad348cd8
supuniuthpala <48378642+supuniuthpala@users.noreply.github.com>
2020-09-16 13:30:23 +0530

    Commit

--- LogAnalyzerApp/src/com/log_Analyzer/Main.java
+++ LogAnalyzerApp/src/com/log_Analyzer/Main.java
@@ -3,6 +3,12 @@ package com.log_Analyzer;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter file path: ");
+        String filepath = input.nextLine();
+        Scanner inputFilePath = new Scanner(new File(filepath));
+        while (inputFilePath.hasNextLine()){
+            System.out.println(inputFilePath.nextLine());
+        }
     }
 }
