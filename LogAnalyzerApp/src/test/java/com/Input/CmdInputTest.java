94aad2b9a34a953d01e3fd112852968a4d250d24
peshaladushyanthika <pdushyanthika@gmail.com>
2020-09-20 16:54:59 +0530

    created EmailInputTest class

--- LogAnalyzerApp/src/test/java/com/Input/CmdInputTest.java
+++ LogAnalyzerApp/src/test/java/com/Input/CmdInputTest.java
@@ -7,9 +7,9 @@ import static org.junit.Assert.*;
 public class CmdInputTest {
     @Test
     public void should_return_filepath() {
-        CmdInput cmd = new CmdInput();
-        String result = cmd.getUserInput();
-
+        String filepath = "D:\\last\\Log_Analyzer\\LogAnalyzerApp\\src\\main\\java\\resources\\sample.txt";
+        String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+        assertTrue(absulutePath.endsWith("D:\\last\\Log_Analyzer\\LogAnalyzerApp\\src\\main\\java\\resources\\sample.txt"));
 
     }
 }
