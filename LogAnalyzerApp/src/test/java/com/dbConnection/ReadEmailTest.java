9df225cffa07b56aed5fd117fa48785ecd3920cf
peshaladushyanthika <pdushyanthika@gmail.com>
2020-09-20 17:15:45 +0530

    LogFileHandlingTest is created

---
+++ LogAnalyzerApp/src/test/java/com/dbConnection/ReadEmailTest.java
@@ -0,0 +1,13 @@
+package com.dbConnection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReadEmailTest {
+    @Test
+    public void shold_return_emailList() {
+        ReadEmail read = new ReadEmail();
+        String result = read.getString();
+    }
+}
